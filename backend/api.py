a41e95f55af68313456fdd887602132d36bff454
maida5 <145588313+maida5@users.noreply.github.com>
2025-08-21 00:21:03 +0000

    api stuff

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
 # import google
 from google import genai
 from dotenv import load_dotenv
@@ -24,29 +24,30 @@ def root():
     console.log("Hello World")
     return {"message": "Hello World"}
 
-@app.post("/")
-async def make_playlist():
-    load_dotenv()
-    api_key = os.getenv('GEMINI_KEY', 'default_key')
+# @app.post("/")
+# async def make_playlist(file: UploadFile = File(...)):
+#     # load_dotenv()
+#     # api_key = os.getenv('GEMINI_KEY', 'default_key')
 
-    with open('test.jpg', 'rb') as f:
-        image_bytes = f.read()
+#     # with open('test.jpg', 'rb') as f:
+#     #     image_bytes = f.read()
 
-    client = genai.Client(api_key=os.getenv('GEMINI_KEY', 'default_key'))
+#     # client = genai.Client(api_key=os.getenv('GEMINI_KEY', 'default_key'))
 
-    # when we do the actual api, make sure to change the mime_type so that it fits with the uploaders image
-    response = client.models.generate_content(
-        model='gemini-2.5-flash',
-        contents=[
-            types.Part.from_bytes(
-            data=image_bytes,
-            mime_type='image/jpeg',
-            ),
-            'You are posting this picture onto social media, such as Instagram. You want to add a song to the image that encapsulates the feeling that the image represents. Give a couple of song options.'
-        ]
-    )
-    console.log(response.text)
-    return jsonable_encoder(response.text)
+#     # # when we do the actual api, make sure to change the mime_type so that it fits with the uploaders image
+#     # response = client.models.generate_content(
+#     #     model='gemini-2.5-flash',
+#     #     contents=[
+#     #         types.Part.from_bytes(
+#     #         data=image_bytes,
+#     #         mime_type='image/jpeg',
+#     #         ),
+#     #         'You are posting this picture onto social media, such as Instagram. You want to add a song to the image that encapsulates the feeling that the image represents. Give a couple of song options.'
+#     #     ]
+#     # )
+#     # console.log(response.text)
+#     # return jsonable_encoder(response.text)
+#     return {file.filename: "File received successfully"}
 
 # import requests
 
