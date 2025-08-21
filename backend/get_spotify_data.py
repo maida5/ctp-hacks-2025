34106cb23a85279aba2a29f147f713c5f6e57f15
maida5 <145588313+maida5@users.noreply.github.com>
2025-08-21 13:52:10 -0400

    saving

--- backend/get_spotify_data.py
+++ backend/get_spotify_data.py
@@ -9,7 +9,7 @@ def set_up_spotify_client():
     load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
 
     # Define the scope: adjust based on what data you need
-    scope = ["user-library-read","user-top-read"]
+    scope = ["user-library-read","user-top-read", "playlist-modify-public"]
     # allows reading your saved tracks
 
     # Authenticate with Spotify using OAuth
@@ -58,5 +58,32 @@ def get_spotify_top_songs():
 def return_spotify_songs():
     sp = set_up_spotify_client()
 
+    artist = "Duran Duran"
+    artist = artist.replace(" ", "%20")
+    song = "Union of the Snake"
+    song = song.replace(" ", "%20")
+    q = f"{artist}%20{song}"
+    track = sp.search(q=q, type='track', limit=1)
+    # print(track)
+    return track['tracks']['items'][0]['uri']
 
+def create_playlist():
+    sp = set_up_spotify_client()
+    user_id = sp.current_user()['id']
+    playlist_name = "My Playlist"
+    playlist_description = "A playlist created by my app"
+    
+    playlist = sp.user_playlist_create(user=user_id, name=playlist_name, public=True, description=playlist_description)
+    print(f"Created playlist: {playlist['name']} with ID: {playlist['id']}")
+    return playlist['id']
+
+def add_tracks_to_playlist(playlist_id, track_ids):
+    sp = set_up_spotify_client()
+    sp.playlist_add_items(playlist_id, track_ids)
+    print(f"Added tracks to playlist {playlist_id}")
+
+print(return_spotify_songs())
+playlist_id = create_playlist()
+song_id = return_spotify_songs()
+add_tracks_to_playlist(playlist_id, [song_id])
 
