2f36a691a2144ddd6238904571e27a2e7d439b20
maida5 <145588313+maida5@users.noreply.github.com>
2025-08-21 11:25:44 -0400

    trying to integrate spotify

--- backend/gemini.py
+++ backend/gemini.py
@@ -2,6 +2,7 @@ from google import genai
 from dotenv import load_dotenv
 from google.genai import types
 import os
+from get_spotify_data import get_spotify_top_songs
 
 load_dotenv()
 api_key = os.getenv('GEMINI_KEY', 'default_key')
@@ -11,6 +12,8 @@ with open('test.jpg', 'rb') as f:
 
 client = genai.Client(api_key=os.getenv('GEMINI_KEY', 'default_key'))
 
+top_songs = get_spotify_top_songs()
+
 # when we do the actual api, make sure to change the mime_type so that it fits with the uploaders image
 response = client.models.generate_content(
 model='gemini-2.5-flash',
@@ -19,7 +22,7 @@ contents=[
     data=image_bytes,
     mime_type='image/jpeg',
     ),
-    'You are posting this picture onto social media, such as Instagram. You want to add a song to the image that encapsulates the feeling that the image represents. Give a couple of song options.'
+    f'You are posting this picture onto social media, such as Instagram. You want to add a song to the image that encapsulates the feeling that the image represents, but you also want it to be representative of you. Here are your last top 50 songs, {top_songs}. Give a couple of song options that follow your music taste but also fit the feeling that the picture gives off.'
 ]
 )
 
--- backend/spotify.py
+++ backend/spotify.py
@@ -2,12 +2,14 @@ import os
 from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import pprint
 
 # Load environment variables from .env
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
 
 # Define the scope: adjust based on what data you need
-scope = "user-library-read"  # allows reading your saved tracks
+scope = ["user-library-read","user-top-read"]
+  # allows reading your saved tracks
 
 # Authenticate with Spotify using OAuth
 sp = spotipy.Spotify(
@@ -20,9 +22,28 @@ sp = spotipy.Spotify(
 )
 
 # Get the user's saved tracks
-results = sp.current_user_saved_tracks(limit=10)
+print("Fetching your saved tracks...")
+results = sp.current_user_top_tracks(limit=50, offset=0, time_range='medium_term')
 
-# Display saved tracks
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(f"{idx+1}. {track['artists'][0]['name']} - {track['name']}")
+# pprint.pp(results['items'][0]['album']['name'])
+# pprint.pp(results['items'][0]['name'])
+# # Display saved tracks
+# for idx, item in enumerate(results['items']):
+#     track = item['track']
+#     print(f"{idx+1}. {track['artists'][0]['name']} - {track['name']}")
+
+top_songs = ""
+
+for item in results['items']:
+    track = item['album']
+    # print(f"Name: {track['name']}")
+    top_songs += f"{item['name']} - "
+    for artists in track['artists']:
+        top_songs += f"{artists['name']}, "
+        # pprint.pp(f"Artist: {artists['name']}")
+
+    top_songs += "\n"
+    
+    # pprint.pp(f"Artist: {track['artists']}")
+print("Top songs:")
+print(top_songs)
