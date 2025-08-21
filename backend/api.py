89c181a42250ba1ee53530529c65fb1809d06937
maida5 <145588313+maida5@users.noreply.github.com>
2025-08-21 09:21:49 -0400

    drop files from gitignore

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, Form
 # import google
 from google import genai
 from dotenv import load_dotenv
@@ -25,12 +25,19 @@ def root():
     return {"message": "Hello World"}
 
 @app.post("/")
-async def make_playlist(file: UploadFile = File(...)):
+async def make_playlist(filedata: str = Form(...)):
+    print(await request.body())  # Log the raw request body
+    print(request.headers)       # Log the request headers
+
     load_dotenv()
     api_key = os.getenv('GEMINI_KEY', 'default_key')
 
-    with open('test.jpg', 'rb') as f:
-        image_bytes = f.read()
+    image_as_bytes = str.encode(filedata)  # convert string to bytes
+    img_recovered = base64.b64decode(image_as_bytes)
+
+    with open(filename, 'wb') as f:
+        # image_bytes = f.read()
+        f.write(img_recovered)
 
     client = genai.Client(api_key=os.getenv('GEMINI_KEY', 'default_key'))
 
@@ -39,15 +46,15 @@ async def make_playlist(file: UploadFile = File(...)):
         model='gemini-2.5-flash',
         contents=[
             types.Part.from_bytes(
-            data=image_bytes,
+            data=f,
             mime_type='image/jpeg',
             ),
             'You are posting this picture onto social media, such as Instagram. You want to add a song to the image that encapsulates the feeling that the image represents. Give a couple of song options.'
         ]
     )
-    console.log(response.text)
+    print(response.text)
     return jsonable_encoder(response.text)
-    return {file.filename: "File received successfully"}
+    # return {file.filename: "File received successfully"}
 
 # import requests
 
