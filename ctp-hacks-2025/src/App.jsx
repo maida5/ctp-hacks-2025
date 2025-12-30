b420faaa4579d5edc3c6ea25215d4befcbc53770
Sanila Chowdhury <sanilachowdhury@gmail.com>
2025-12-30 18:17:26 -0500

    change app name

--- ctp-hacks-2025/src/App.jsx
+++ ctp-hacks-2025/src/App.jsx
@@ -5,7 +5,7 @@ import ImageUploader from './ImageUploader'
 function App() {
   return (
     <div className="animated-background">
-      <h1>Project Aura</h1>
+      <h1>AuraTracks</h1>
       <h2>Please upload an image</h2>
       <div className="card">
         <ImageUploader />
